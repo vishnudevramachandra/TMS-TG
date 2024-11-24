fix: computation of significantly active neuron earlier utilised firing activity in the block with condition CoilDir == 'ML' and StimHem == RecHem, but now the block with the highest firing rate is used.

figures/figure1/figure1a.py
@@ -66,21 +66,22 @@ def plot(tms, activeNeus, kind=None, colParams=None, xlim=None, epochAndNeuron=N
     assert type(colParams) == tuple or colParams is None, 'colNames has to be a list of dictionary items or None'
     assert kind in ('rasterOnly', 'rasterAndPsfr', 'rasterAndPopulationAvgFR'), 'unknown input for parameter "kind"'
 
-    # Conditions for raster row categories
+    # Define raster categories to plot in separate rows
     rasterRowConds = ({'selectionParams': {'Epoch': dict(zip_longest(EPOCHISOLATORS, [None, ])), 'MT': '<1'}},
                       {'selectionParams': {'Epoch': dict(zip_longest(EPOCHISOLATORS, [None, ])), 'MT': '==1'}},
                       {'selectionParams': {'Epoch': dict(zip_longest(EPOCHISOLATORS, [None, ])), 'MT': '>1'}})
 
-    # Condition for epochs with 'MT' == 0
+    # Define condition for zero MT
     zeroMTCond = {'selectionParams': {'Epoch': dict(zip_longest(EPOCHISOLATORS, [None, ])), 'MT': '==0'}}
 
+    # Set plot styles
     plt.style.use('default')
     colorsPlt = ('C0', 'C1', 'C2')
     ylim = [np.inf, 0]
     fig, ax = construct_subplots(colParams, kind)
     animalNumsEpochNumsAndActiveNeuronNums_perCol = list()
 
-    # Iterate over columns
+    # Iterate over column parameters and do the plot
     for colIdx in range(len(colParams)):
         colName = ascertain_colName_from_colParams(colParams[colIdx])
         tms.analysis_params = copy.deepcopy(colParams[colIdx])
@@ -113,13 +114,11 @@ def plot(tms, activeNeus, kind=None, colParams=None, xlim=None, epochAndNeuron=N
                                        mean(tms.analysis_params['peristimParams']['baselinetimeWin']),
                                        tms.analysis_params['peristimParams']['baselinetimeWin'][1],
                                        tms.analysis_params['peristimParams']['trigger']))
-            ps_T_corrected = tms.analysis_params['peristimParams']['timeWin'][0] + ps_T
 
         # Statistics
         animalNumsEpochNumsAndActiveNeuronNums_perCol.append((
             np.unique([item[0] for item in epochIndices]).size,
-            (epochIndices),
-            [activeNeus[item].sum() for item in epochIndices]
+            list(zip((epochIndices), [activeNeus[item].sum() for item in epochIndices]))
         ))
 
         # --------------------------------------------------------------------------------------------------------------
@@ -134,6 +133,7 @@ def plot(tms, activeNeus, kind=None, colParams=None, xlim=None, epochAndNeuron=N
                 idx = np.where(blockIdx)[0][sampleBlocksinfo.loc[blockIdx, 'no. of Trigs'].argmax()]
                 blockIdx.iloc[np.setdiff1d(np.where(blockIdx)[0], idx)] = False
 
+            # If first column in being plotted then set the ylabel
             if colIdx == 0:
                 ax[i][colIdx].set_ylabel(
                     f'MT = {sampleBlocksinfo[blockIdx]["MT"].values[0]}\nTrials (N)', fontsize=8)
@@ -150,7 +150,7 @@ def plot(tms, activeNeus, kind=None, colParams=None, xlim=None, epochAndNeuron=N
                 selectPSFR = samplePSFR[np.nonzero(blockIdx)[0][0]]
                 plot_MeanAndSEM(selectPSFR.mean(axis=0)[:, neuIdx],
                                 selectPSFR.std(axis=0)[:, neuIdx] / np.sqrt(selectPSFR.shape[0]),
-                                ps_T_corrected,
+                                ps_T,
                                 ax[rowIdx][colIdx],
                                 colorsPlt[i],
                                 f'MT = {sampleBlocksinfo[blockIdx]["MT"].mean()}')
@@ -160,7 +160,7 @@ def plot(tms, activeNeus, kind=None, colParams=None, xlim=None, epochAndNeuron=N
             # ----------------------------------------------------------------------------------------------------------
             # plot population average firing rate
             if kind == 'rasterAndPopulationAvgFR':
-                plot_populationAvgFR(samplePSFR, ps_T_corrected, selectBlocksinfo, zeroMTCond, activeNeus,
+                plot_populationAvgFR(samplePSFR, ps_T, selectBlocksinfo, zeroMTCond, activeNeus,
                                      rasterRowConds, ax[rowIdx][colIdx], colorsPlt,
                                      [item['selectionParams']['MT'] for item in rasterRowConds])
 
@@ -193,8 +193,9 @@ if __name__ == '__main__':
     epochs = (
         {'selectionParams': {'Epoch': {key: value for key, value in zip_longest(keys, ('MC', 'L5'))}}},
         {'selectionParams': {'Epoch': {key: value for key, value in zip_longest(keys, ('SC', 'L5'))}}},
-        {'selectionParams': {'Epoch': {key: value for key, value in zip_longest(keys, ('VC', 'L5'))}}},
+
     )
+    #{'selectionParams': {'Epoch': {key: value for key, value in zip_longest(keys, ('VC', 'L5'))}}},
 
     # Load TMS data from the specified Excel file
     animalList = r'G:\Vishnu\data\TMSTG\animalList.xlsx'
@@ -211,10 +212,14 @@ if __name__ == '__main__':
     plotKinds = ('rasterOnly', 'rasterAndPsfr', 'rasterAndPopulationAvgFR')
 
     # Plot raster and peri-stimulus firing rate (PSFR) for specified epochs and neurons
-    plot(tms, activeNeu, kind=plotKinds[1], colParams=epochs, xlim=[-50, 350], epochAndNeuron=
-         ({'epochIndex': ('20200705', 'MC', 'L5', 'opposite', 'none', '1467'), 'neuIdx': 5},
-          {'epochIndex': ('20200705', 'SC', 'L5', 'same', 'none', '1496'), 'neuIdx': 4},
-          {'epochIndex': ('20200705', 'VC', 'L5', 'same', 'none', '1404'), 'neuIdx': 1}))
+    # plot(tms, activeNeu, kind=plotKinds[1], colParams=epochs, xlim=[-50, 350], epochAndNeuron=
+    #      ({'epochIndex': ('20200705', 'MC', 'L5', 'opposite', 'none', '1467'), 'neuIdx': 5},
+    #       {'epochIndex': ('20200705', 'SC', 'L5', 'same', 'none', '1496'), 'neuIdx': 4},
+    #       {'epochIndex': ('20200705', 'VC', 'L5', 'same', 'none', '1404'), 'neuIdx': 1}))
+
+    plot(tms, activeNeu, kind=plotKinds[1], colParams=epochs, xlim=[-2, 8], epochAndNeuron=
+         ({'epochIndex': ('20170201', 'MC', 'L5', 'none', 'none', '1407'), 'neuIdx': 0},
+          {'epochIndex': ('20170201', 'SC', 'L5', 'none', 'none', '1307'), 'neuIdx': 0}))
 
     # Reset the number of threads for Numba to default
     nb.set_num_threads(nb.config.NUMBA_DEFAULT_NUM_THREADS)

figures/helper_figs.py
@@ -74,7 +74,8 @@ def selectNeuron(activeNeu):
 
 # Randomly select an epoch and a neuron for plotting
 def selectEpochAndNeuron(epochAndNeuron, tms, epochIndices, activeNeus, colParam):
-    # Randomly sample an epoch that has at least one active neuron for plotting if EpochAndNeuron is None
+    # Randomly sample an epoch that has at least one active neuron for plotting if the choice of Epoch is unspecified,
+    # i.e., if EpochAndNeuron is None
     sampleActiveNeus = [False, ]
     while not any(sampleActiveNeus):
         sampleEpochIndex = epochIndices[np.random.choice(len(epochIndices))] \
@@ -88,7 +89,7 @@ def selectEpochAndNeuron(epochAndNeuron, tms, epochIndices, activeNeus, colParam
             assert any(sampleActiveNeus), f'Epoch {sampleEpochIndex} does not have any active neurons'
             break
 
-    # Randomly select a neuron for plotting raster
+    # Randomly select a neuron for plotting raster if choice of Neuron is unspecified, i.e., if EpochAndNeuron is None
     neuIdx = np.random.choice(sampleActiveNeus.nonzero()[0]) \
         if epochAndNeuron is None or 'neuIdx' not in epochAndNeuron.keys() else epochAndNeuron['neuIdx']
 

tms_tg.py
@@ -1,10 +1,12 @@
+import itertools
+
 import numpy as np
 import numba as nb
 import pandas as pd
 from statistics import mean, mode
 import os
 from typing import Optional, Any
-from itertools import zip_longest
+from itertools import zip_longest, product
 from functools import lru_cache, cached_property
 import scipy
 
@@ -192,14 +194,14 @@ def hem_CoilDir_selector(blocksinfo, index, hem, cDir):
 
 
 # Select a block from blocksinfo DataFrame based on the specified epoch and amplitude criteria.
-def block_selector(blocksinfo: pd.DataFrame, epoch: tuple, amplitude: Optional[str] = 'MT') \
+def block_selector(blocksinfo: pd.DataFrame, epoch: tuple, trialwise_PS_FR: list, amplitude: Optional[str] = 'MT') \
         -> np.ndarray[Any, np.dtype[np.float64]]:
     """
     Find out the index of the block that matches the given amplitude for a particular epoch. In case the epoch
     contains multiple blocks with same amplitude as it happens when injections are done, then select the one prior
     to injection, if it exists, otherwise select the last 'Post' injection block. In the additional case, where the
-    epoch contains multiple blocks with same amplitude due to different combinations of StimHem and CoilDir, select the
-    one with CoilDir == 'ML' and StimHem == RecHem.
+    epoch contains multiple blocks with same amplitude due to different combinations of StimHem ('RH', 'LH')
+    and CoilDir ('ML', 'LM', etc.), select the one with the highest firing rate.
 
     Parameters
     ----------
@@ -250,29 +252,34 @@ def block_selector(blocksinfo: pd.DataFrame, epoch: tuple, amplitude: Optional[s
     # Handle cases where no blocks meet the criteria
     if not any(index):
         # Here its considered that only one injection condition was experimentally tested at a time, i.e., no overlaps,
-        # so first extract it. Then either the last 'Post' condition pertaining to stimulus 'amplitude' is used as index
-        # or the last 'Post' condition under the criteria CoilDir == 'ML' and StimHem == RecHem is used as the index
+        # so first extract it. Then either the last 'Post' condition pertaining to stimulus 'amplitude', when only one
+        # CoilDir & StimHem was tested, is used as index or in case of multiple combinations of CoilDir & Stim, the
+        # 'Post' condition towards the end that has the highest firing rate is used as the index
         injTypesPostRelation = blocksinfo.loc[epoch, injTypesPresent].apply(
             lambda x: x.str.contains('post', case=False)).any()
         injWithPost = injTypesPostRelation.index[injTypesPostRelation.values]
         if len(injWithPost) > 0:
-            postT = blocksinfo[injWithPost.item()].str.extract('(\d+)').astype('float64')
+            postT = blocksinfo[injWithPost.item()].str.extract('(\d+)').astype('float64').squeeze()
             postT.loc[list(set(blocksinfo.index.unique()) - {epoch})] = 0.0
             postT[~mtIdx.to_numpy()] = 0.0
-            bIdx = hem_CoilDir_selector(blocksinfo, np.nonzero(postT)[0], 'same', 'ML')
-            if any(bIdx):
-                postT.iloc[np.nonzero(postT)[0][~bIdx]] = 0.0
-            index = np.nonzero(postT == postT.max())[0]
+
+            firingrate = 0.0
+            # Iterate over every combination of CoilDir & Stim
+            for item in product(blocksinfo['StimHem'].unique(), blocksinfo['CoilDir'].unique()):
+                selIdx = np.nonzero((blocksinfo['StimHem'] == item[0]) & (blocksinfo['CoilDir'] == item[1]))[0]
+                lastBlockIdx = selIdx[postT.iloc[selIdx].argmax()]
+                # update Index to point to the block having the highest firing rate
+                if trialwise_PS_FR[lastBlockIdx].sum() > firingrate:
+                    firingrate = trialwise_PS_FR[lastBlockIdx].sum()
+                    index = lastBlockIdx
+
+            index = np.array([index])
 
     # Handle cases where multiple blocks meet the criteria
     elif len(index) > 1:
-        # Find the index that corresponds to criteria CoilDir == 'ML' and StimHem == RecHem
-        bIdx = hem_CoilDir_selector(blocksinfo, index, 'same', 'ML')
-        if any(bIdx):
-            index = index[bIdx]
-        else:
-            # if criteria CoilDir == 'ML' and StimHem == RecHem is non-existent, pick the first block.
-            index = index[0]
+        # Find the index that corresponds to the block having the highest firing rate
+        maxIdx = np.array([trialwise_PS_FR[item].sum() for item in index]).argmax(keepdims=True)
+        index = index[maxIdx]
 
     return index
 
@@ -485,11 +492,11 @@ class TMSTG(object):
             else:
                 self.blocksinfo.iat[idx, psActivityIdx] = {params: [tmp_ps_FR, ps_T]}
 
-            # Reset filter_blocks
-            self.filter_blocks = None
-
             # Append computed firing rate to the list
             ps_FR.append(tmp_ps_FR)
+
+        # Reset filtering of blocks
+        self.filter_blocks = None
 
         return ps_FR, ps_T
 
@@ -641,14 +648,14 @@ class TMSTG(object):
         selectBlocksinfo, selectBlocksinfoIdx = self.filter_blocks
         epochs = selectBlocksinfo.index.unique()
 
-        # Compute whether neurons are significantly active at MT stimulation (if non-existent than Max stimulation,
+        # Find out whether neurons are significantly active at MT stimulation (if non-existent then Max stimulation,
         # for silencing conditions the last MT stimulation)
         activeNeus = pd.Series(np.empty(shape=epochs.shape), index=epochs)
         for epoch in epochs:
-            index = block_selector(selectBlocksinfo, epoch, amplitude='MT')
+            index = block_selector(selectBlocksinfo, epoch, trialwise_PS_FR, amplitude='MT')
             assert len(index) < 2, f'epoch {epoch} has more than one MT cond, not correct'
             if len(index) == 0:
-                index = block_selector(selectBlocksinfo, epoch, amplitude='maximum')
+                index = block_selector(selectBlocksinfo, epoch, trialwise_PS_FR, amplitude='maximum')
                 assert len(index) < 2, f'epoch {epoch} has more than one MT cond, not correct'
             postStimFR = trialwise_PS_FR[index.item()]
             baselineFR = trialwise_BL_FR[index.item()]